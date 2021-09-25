c104d5d90661454f259f9c4420dbcc53f3f8fb05
Neel <nprosofficial@gmail.com>
2021-09-25 14:44:55 +0530

    added form with state

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The Good Samaritan',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
@@ -63,7 +63,7 @@ class SecondRoute extends StatelessWidget {
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => FindRoute()),
+                    MaterialPageRoute(builder: (context) => FindVolunteerForm()),
                   );
                 },
               ),
@@ -116,40 +116,70 @@ class VolunteerRoute extends StatelessWidget {
   }
 }
 
-class FindRoute extends StatelessWidget {
-  const FindRoute({Key? key}) : super(key: key);
+class FindVolunteerForm extends StatelessWidget {
+  const FindVolunteerForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Create an Entry"),
-        automaticallyImplyLeading: false,
+    const appTitle = 'Find Volunteers';
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(25),
-              child: FlatButton(
-                child: const Text('I\'m a Volunteer', style: TextStyle(fontSize: 20.0),),
-                color: Colors.green,
-                textColor: Colors.white,
-                onPressed: () {},
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(25),
-              child: FlatButton(
-                child: const Text('I\'m Looking for Volunteers', style: TextStyle(fontSize: 20.0),),
-                color: Colors.green,
-                textColor: Colors.white,
-                onPressed: () {},
-              ),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
         ),
+        body: const MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
   }
